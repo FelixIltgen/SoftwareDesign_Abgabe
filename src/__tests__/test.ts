35072c432f649053f97a00aecb4f50581160120f
Felix <felix.horst.iltgen@hs-furtwangen.de>
2022-02-13 22:08:43 +0100

    Added regEx

--- src/__tests__/test.ts
+++ src/__tests__/test.ts
@@ -1,5 +1,22 @@
-describe("this is a simple test!", () => {
-  test("Check test function!", () => {
-    expect(true).toBeTruthy();
-  });
+import {User} from "../classes/User";
+let user: User = new User;
+
+describe("Check name of user", () => {
+
+  let invalid: Array<string> = ["Peter#Pan","#%dude","Ben - Holger"];
+  let valid: Array<string> = ["FelixIltgen","MaxMueller6"];
+
+  for(let nameOfUser of invalid){
+    test("Check invalid username", () => {
+      expect(user.regEx(nameOfUser)).toBeFalsy();
+    });
+  }
+
+  for(let nameOfUser of valid){
+    test("Check valid username", () => {
+      expect(user.regEx(nameOfUser)).toBeTruthy();
+    });
+  }
+
+  
 });
--- src/classes/User.ts
+++ src/classes/User.ts
@@ -15,7 +15,7 @@ export class User {
   public globalUserName: string = "";
   public allUserInfo: userData[] = FileHandler.readJsonFile("json/user.json");
   public data: userData = { username: "", password: "" };
-  
+
   public async userRegitser(): Promise<void> {
 
     console.log("Wilkommen bei der Registrierung!");
@@ -24,37 +24,43 @@ export class User {
     let username = await Console.userQuestion("Geben Sie ein Benutzername ein!");
     let userPassword = await Console.userQuestionPw("Geben Sie ein Passwort ein!");
 
-    //check if the input is filled with admin values
-    if (username.value == "Admin" && userPassword.value == "123") {
-      console.log("HALllLooo?!?!, als Admin musst du dich nicht Registrieren, los, auf zur Anmeldung!");
-    
-    } else {
-      //check all usernames if input is equal
-      for (let i = 0; i < this.allUserInfo.length; i++) {
-        if (this.allUserInfo[i].username == username.value) {
+    if (this.regEx(username.value)) {
 
-          this.checkUsername = true;
-          break;
-
-        } else {
-          this.checkUsername = false;
-        }
-      }
-      //check value of checkUsername
-      if (this.checkUsername == false) {
-        //write values in interface and json
-        this.data.username = username.value;
-        this.data.password = userPassword.value;
-        this.allUserInfo.push(this.data);
-        FileHandler.writeJsonFile("json/user.json", this.allUserInfo);
-        //answer user
-        console.log("Benutzername und Passwort wurden gespeichert!");
-        console.log("Wilkommen " + username.value + " Sie sind jetzt Registriert, viel Spaß");
-        this.globalUserName = username.value;
+      //check if the input is filled with admin values
+      if (username.value == "Admin" && userPassword.value == "123") {
+        console.log("HALllLooo?!?!, als Admin musst du dich nicht Registrieren, los, auf zur Anmeldung!");
 
       } else {
-        console.log("Der Benutzername ist bereits vergeben!");
+        //check all usernames if input is equal
+        for (let i = 0; i < this.allUserInfo.length; i++) {
+          if (this.allUserInfo[i].username == username.value) {
+
+            this.checkUsername = true;
+            break;
+
+          } else {
+            this.checkUsername = false;
+          }
+        }
+        //check value of checkUsername
+        if (this.checkUsername == false) {
+          //write values in interface and json
+          this.data.username = username.value;
+          this.data.password = userPassword.value;
+          this.allUserInfo.push(this.data);
+          FileHandler.writeJsonFile("json/user.json", this.allUserInfo);
+          //answer user
+          console.log("Benutzername und Passwort wurden gespeichert!");
+          console.log("Wilkommen " + username.value + " Sie sind jetzt Registriert, viel Spaß");
+          this.globalUserName = username.value;
+
+        } else {
+          console.log("Der Benutzername ist bereits vergeben!");
+        }
       }
+    }else{
+      console.log("Bitte nur Buchstaben und Zahlen verwenden");
+      this.userRegitser();
     }
   }
 
@@ -71,7 +77,7 @@ export class User {
 
       console.log("Sie sind als Admin eingelogt");
       this.globalUserName = username.value;
-    
+
     } else {
 
       // check all user inforamtion
@@ -87,7 +93,7 @@ export class User {
       }
       //check value of checkUsername
       if (this.checkUsername == true) {
-        
+
         console.log("Hallo " + username.value + ", willkommen zurück, wie können wir Ihnen helfen!");
         //set user name
         this.globalUserName = username.value;
@@ -99,8 +105,13 @@ export class User {
     }
   }
 
-  public getUsername(): string{
+  public getUsername(): string {
     //return name of user
     return this.globalUserName;
+  }
+  public regEx(name: string): boolean {
+
+    let regExUsername: RegExp = /^[a-zA-Z0-9]{1,11}$/;
+    return regExUsername.test(name);
   }
 }
